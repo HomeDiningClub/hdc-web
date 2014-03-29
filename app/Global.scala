aa0c63dfd8b542e11bc2ac0d9ff8a868fb276c1e
Fredrik <fredrik.trott@gmail.cm>
2014-03-29 22:13:01 +0100

    Experiment med Noe4J utan att komma någonstans. Lagt till label på fält.

--- app/Global.scala
+++ app/Global.scala
@@ -1,30 +1,30 @@
-import play.api._
+import play.Application
+import play.GlobalSettings
+import play.Logger
 import com.tinkerpop.blueprints.Vertex
 import com.wingnest.play2.frames.GraphDB
-import play.Logger
+import Global._
+//remove if not needed
+import scala.collection.JavaConversions._
 
+// Java2Scala
+// http://javatoscala.com/
 
-object Global extends GlobalSettings {
+object Global {
 
   private var init: Boolean = false
+}
 
-  override def onStart(app: Application) {
-    //Logger.info("Application has started")
+class Global extends GlobalSettings {
 
+  override def  onStart(app: Application) {
     try {
       if (!init) {
-       //GraphDB.createKeyIndex("className", Vertex)
+        GraphDB.createKeyIndex("className", classOf[Vertex])
         init = true
       }
+    } catch {
+      case e: java.lang.UnsupportedOperationException => Logger.info(e.getMessage)
     }
-  //  catch {
-      //case e: UnsupportedOperationException => Logger.info("test")
-  //  }
-
   }
-
-  override def onStop(app: Application) {
-    //Logger.info("Application shutdown...")
-  }
-
 }
