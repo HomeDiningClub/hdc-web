9bb01648486b2dee11193f5d4ad8fab24f5343c7
Fredrik <fredrik.trott@gmail.cm>
2014-07-07 08:54:29 +0200

    Added tagGroupName for selection on a group of tagnames mening differnet lists

--- app/repositories/TagWordRepository.scala
+++ app/repositories/TagWordRepository.scala
@@ -1,8 +1,13 @@
 package repositories
 
 import models.profile.TagWord
+import org.springframework.data.neo4j.annotation.Query
 import org.springframework.data.neo4j.repository.GraphRepository
 
+
 trait TagWordRepository extends GraphRepository[TagWord] {
+
+  @Query("start up=node:TagWord(tagGroupName={0}) return up")
+  def getGroup(groupName: String ): List[TagWord]
 
 }
