025b233f25f45cc10eef862b58481d2868965bd4
Fredrik <fredrik.trott@gmail.cm>
2014-07-07 08:42:38 +0200

    Added tagGroupName for selection on a group of tagnames mening differnet lists

--- app/models/profile/TagWord.java
+++ app/models/profile/TagWord.java
@@ -2,13 +2,19 @@ package models.profile;
 
 
 import models.base.AbstractEntity;
+import org.springframework.data.neo4j.annotation.Indexed;
 import org.springframework.data.neo4j.annotation.NodeEntity;
+import org.springframework.data.neo4j.support.index.IndexType;
 
 @NodeEntity
 public class TagWord  extends AbstractEntity {
-    public String tagName = "";
-    public String tagId   = "";
-    public String orderId = "";
+
+    public String tagName      = "";
+    public String tagId        = "";
+    public String orderId      = "";
+
+    @Indexed(indexType = IndexType.FULLTEXT, indexName = "TAGWORD_GROUP_NAME")
+    public String tagGroupName = "";
 
 
     public TagWord() {
@@ -17,8 +23,16 @@ public class TagWord  extends AbstractEntity {
 
     public TagWord(String tagName, String tagId, String orderId) {
 
-        this.tagName    = tagName;
-        this.tagId      = tagId;
-        this.orderId    = orderId;
+        this.tagName        = tagName;
+        this.tagId          = tagId;
+        this.orderId        = orderId;
+    }
+
+    public TagWord(String tagName, String tagId, String orderId, String tagGroupName) {
+
+        this.tagName        = tagName;
+        this.tagId          = tagId;
+        this.orderId        = orderId;
+        this.tagGroupName   = tagGroupName;
     }
 }
