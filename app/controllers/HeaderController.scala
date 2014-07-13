fe196f54c3d01f73f17b73bf8e35a942cdd9025c
MagnusWallin <magnus@devitec.se>
2014-07-14 00:46:36 +0200

    Implicit user test

--- app/controllers/HeaderController.scala
+++ app/controllers/HeaderController.scala
@@ -2,8 +2,11 @@ package controllers
 
 import play.api.mvc._
 import play.api.i18n.Messages
+import securesocial.core.SecureSocial
+import play.api.Logger
+import models.UserCredential
 
-object HeaderController extends Controller {
+object HeaderController extends Controller with SecureSocial {
 
   // Name, Title, Href, Class
   val menuItemsList = Seq[(String,String,Call,String)](
@@ -16,11 +19,20 @@ object HeaderController extends Controller {
   )
 
   // Name, Title, Href, Class, Extra HTML
-  val quickLinkList = Seq[(String,String,Call,String,String)](
+  var quickLinkList = Seq[(String,String,Call,String,String)](
     (Messages("header.link.host-profile"), Messages("header.link.host-profile"), routes.HostController.index, "", ""),
     (Messages("header.link.inbox"), Messages("header.link.inbox"), routes.HostController.index, "", "<span class=\"badge\">0</span>"),
     (Messages("header.link.login"), Messages("header.link.login"), securesocial.controllers.routes.LoginPage.login, "", "")
   )
+
+  implicit def getUserFromRequest(implicit request: play.api.mvc.RequestHeader): Option[UserCredential] = {
+    SecureSocial.currentUser.map {
+      u =>
+        Logger.debug("Debug:" + u.fullName)
+        Some(u.asInstanceOf[UserCredential])
+    }
+    None
+  }
 
   def index = {
     views.html.header.header(menuItemsList, quickLinkList)
